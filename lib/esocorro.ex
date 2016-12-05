79f151ba61f2b0d09fbe34db0a18af2d8657a303
Julián Molina <phalcondroid@gmail.com>
2016-12-05 16:08:44 -0500

    setting connection

--- lib/esocorro.ex
+++ lib/esocorro.ex
@@ -3,8 +3,10 @@ defmodule Socorro do
     use Application
     
     alias Socorro.Core.{ Pool }
+    alias Socorro.Db.MongoPool
 
     def start(_type, _args) do
+        MongoPool.connect()
         Pool.start_link
     end
 
